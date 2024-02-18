d350f900d4502be16e88c0f75ce7c24109a7f6c3
Sai Preeti <psaipreeti@gmail.com>
2024-02-18 15:27:15 +0530

    added few pages

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for
 import requests
 import random
+import os
 
 app = Flask(__name__,template_folder='templates')
 
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/',methods=['GET'])
-def home():
-    return render_template('HomePage.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return render_template('preview.html', filename=filename)
+    return redirect(request.url)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return 'Prediction page'
 
 if __name__ == '__main__':
     app.run(debug=True)
